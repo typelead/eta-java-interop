36705ab57ade6049447ee7ba658719380ac216bb
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2018-02-10 02:34:19 +0530

    Added Cloneable

--- src/Interop/Java/Lang.hs
+++ src/Interop/Java/Lang.hs
@@ -5,3 +5,10 @@ import Java
 
 data ClassLoader = ClassLoader @java.lang.ClassLoader
   deriving Class
+
+-- Start java.lang.Cloneable
+
+data Cloneable = Cloneable @java.lang.Cloneable
+  deriving Class
+
+-- End java.lang.Cloneable
