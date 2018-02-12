49d029d5d11c375172699beec44e69fe001c49b3
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2018-02-12 21:38:54 +0530

    Updated java.lang

--- src/Interop/Java/Lang.hs
+++ src/Interop/Java/Lang.hs
@@ -2,6 +2,9 @@
 module Interop.Java.Lang where
 
 import Java
+import Java.Exception
+import Data.Typeable
+
 
 data ClassLoader = ClassLoader @java.lang.ClassLoader
   deriving Class
@@ -26,3 +29,26 @@ data Appendable = Appendable @java.lang.Appendable
   deriving Class
 
 -- End java.lang.Appendable
+
+-- Start java.lang.SecurityException
+
+data SecurityException = SecurityException @java.lang.SecurityException
+  deriving (Class, Typeable)
+
+type instance Inherits SecurityException = '[RuntimeException]
+
+-- End java.lang.SecurityException
+
+-- Start java.lang.AutoCloseable
+
+data AutoCloseable = AutoCloseable @java.lang.AutoCloseable
+  deriving Class
+
+-- End java.lang.AutoCloseable
+
+-- Start java.util.Observable
+
+data Observable = Observable @java.util.Observable
+  deriving Class
+
+-- End java.util.Observable
